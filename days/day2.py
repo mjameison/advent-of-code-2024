aaf707840bcddd19a328fc334f42833c9b00b90c
Matthew Jameison <matthewjam46@gmail.com>
2024-12-02 20:53:49 +0000

    Reduce memory use

--- days/day2.py
+++ days/day2.py
@@ -11,29 +11,27 @@ def is_report_valid(report: list[int], *, increasing: bool) -> tuple[bool, bool]
             diff = -diff
         return MIN_CHANGE <= diff <= MAX_CHANGE
 
-    valid_no_remove = [True, is_valid_transition(report[0], report[1])]
-    valid_with_remove = [True, True]
-    for index in range(2, len(report)):
-        base_valid = valid_no_remove[index - 1] and is_valid_transition(
-            report[index - 1], report[index]
-        )
-        valid = (
-            valid_with_remove[index - 1]
-            and is_valid_transition(report[index - 1], report[index])
-        ) or (
-            valid_no_remove[index - 2]
-            and is_valid_transition(report[index - 2], report[index])
-        )
-        valid_no_remove.append(base_valid)
-        valid_with_remove.append(valid)
+    # a_x is all included to x back is valid
+    a_2, a_1 = True, is_valid_transition(report[0], report[1])
+    # s_x is all included with 1 skip to x back is valid
+    s_1 = True
 
-    return valid_no_remove[-1], valid_with_remove[-1] or valid_no_remove[-2]
+    for index in range(2, len(report)):
+        # t_x is if the transition from x back to current valid
+        t_1 = is_valid_transition(report[index - 1], report[index])
+        t_2 = is_valid_transition(report[index - 2], report[index])
+        a_0 = a_1 and t_1
+        s_0 = (s_1 and t_1) or (a_2 and t_2)
+
+        a_2, a_1 = a_1, a_0
+        s_1 = s_0
+
+    return a_1, s_1 or a_2
 
 
 def run(path: str) -> None:
     safe_line_count = 0
     fudged_line_count = 0
-    bf_count = 0
 
     with Path(path).open() as file:
         for line in file:
@@ -47,4 +45,3 @@ def run(path: str) -> None:
 
     print(f"Part 1: {safe_line_count}")
     print(f"Part 2: {fudged_line_count}")
-    print(f"Part 2: {bf_count}")
